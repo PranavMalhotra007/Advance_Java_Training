e965e559e3a3d802efaa48906ce91413cd6ce1d6
Pranav Malhotra <malhotrapranav72003@gmail.com>
2026-03-10 12:17:58 +0530

    update

--- Spring-Boot/payment/src/main/java/com/lpu/payment/PaymentApplication.java
+++ Spring-Boot/payment/src/main/java/com/lpu/payment/PaymentApplication.java
@@ -2,7 +2,8 @@ package com.lpu.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PaymentApplication {
 
