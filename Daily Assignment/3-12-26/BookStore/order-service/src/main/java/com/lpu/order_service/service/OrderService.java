26ebb433342b470a6ec501a2e572b1d1c7caaabe
Pranav Malhotra <malhotrapranav72003@gmail.com>
2026-03-12 14:24:01 +0530

    update

---
+++ Daily Assignment/3-12-26/BookStore/order-service/src/main/java/com/lpu/order_service/service/OrderService.java
@@ -0,0 +1,36 @@
+package com.lpu.order_service.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.lpu.order_service.entity.BookDTO;
+import com.lpu.order_service.entity.Order;
+import com.lpu.order_service.repository.OrderRepository;
+
+@Service
+public class OrderService {
+	private OrderRepository orderRepository;
+
+	public OrderService(OrderRepository orderRepository) {
+		super();
+		this.orderRepository = orderRepository;
+	}
+	public Order saveOrder(Order order, BookDTO bookDTO) {
+		order.setTotalprice(bookDTO.getPrice()*order.getQuantity());
+		return orderRepository.save(order);
+	}
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	public Order findById(int id) {
+		return orderRepository.findById(id).get();
+	}
+	public String deleteOrder(int id) {
+		orderRepository.deleteById(id);
+		return "Order deleted";
+	}
+	public Order updateOrder(int id, Order order) {
+		return orderRepository.save(order);
+	}
+}
